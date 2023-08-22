5d32b9740df6804170cb15a6c2b0dbd4d179c24d
Julian <Julian@test.com>
2023-08-22 17:00:43 -0400

    Add source files

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿// Code: Calculator.cs
+
+public class Calculator
+{
+    public int Add(int a, int b)
+    {
+        return a + b;
+    }
+}
+// Unit Tests: CalculatorTests.cs
+
+using NUnit.Framework;
+
+[TestFixture]
+public class CalculatorTests
+{
+    private Calculator calculator;
+
+    [SetUp]
+    public void Setup()
+    {
+        calculator = new Calculator();
+    }
+
+    [Test]
+    public void Add_TwoNumbers_ReturnsSum()
+    {
+        // Arrange
+        int a = 5;
+        int b = 10;
+
+        // Act
+        int result = calculator.Add(a, b);
+
+        // Assert
+        Assert.AreEqual(15, result);
+    }
+
+    [Test]
+    public void Add_NegativeNumbers_ReturnsSum()
+    {
+        // Arrange
+        int a = -8;
+        int b = -5;
+
+        // Act
+        int result = calculator.Add(a, b);
+
+        // Assert
+        Assert.AreEqual(-13, result);
+    }
+}
